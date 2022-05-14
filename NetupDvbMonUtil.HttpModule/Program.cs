967b6868d8fa6a280da2bd7fdb387901a71d84e9
Zakharov Svyatoslav <szakharov@hrsinternational.com>
2022-05-14 21:11:17 +0300

    Init HttpModule Solution

---
+++ NetupDvbMonUtil.HttpModule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetupDvbMonUtil.HttpModule
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
